e988bfd3cb2d63dbd571181859de003fe884e80c
zeidali <122940414+zeidali@users.noreply.github.com>
2025-06-29 00:07:10 +0300

    Update extractor.py
    some changes to make it work on Termux and Python 3.12

--- core/extractor.py
+++ core/extractor.py
@@ -1,31 +1,24 @@
 import warnings
 warnings.filterwarnings("ignore", category=SyntaxWarning)
-
 import re
 
 def param_extract(response, level, black_list, placeholder):
-    ''' 
-    Function to extract URLs with parameters (ignoring the black list extension)
-    regexp : r'.*?:\/\/.*\?.*=[^$]'
-    '''
-    
-    # Using raw string to avoid invalid escape sequence warning
-    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*=[^$]', response)))  # Raw string literal (r'...')
+    parsed = list(set(re.findall(r'.*?:\/\/.*\?.*=[^$]', response)))
     final_uris = []
-    
+
     for i in parsed:
         delim = i.find('=')
         second_delim = i.find('=', i.find('=') + 1)
-        
-        if len(black_list) > 0:
-            words_re = re.compile("|".join(black_list))
+
+        if black_list:
+            words_re = re.compile("|".join(map(re.escape, black_list)))
             if not words_re.search(i):
-                final_uris.append((i[:delim+1] + placeholder))
-                if level == 'high':
-                    final_uris.append(i[:second_delim+1] + placeholder)
+                final_uris.append(i[:delim + 1] + placeholder)
+                if level == 'high' and second_delim > delim:
+                    final_uris.append(i[:second_delim + 1] + placeholder)
         else:
-            final_uris.append((i[:delim+1] + placeholder))
-            if level == 'high':
-                final_uris.append(i[:second_delim+1] + placeholder)
+            final_uris.append(i[:delim + 1] + placeholder)
+            if level == 'high' and second_delim > delim:
+                final_uris.append(i[:second_delim + 1] + placeholder)
 
     return list(set(final_uris))
