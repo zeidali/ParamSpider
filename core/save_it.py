6214ec7a02d3ec734585751ee4878514eccd12d0
zeidali <122940414+zeidali@users.noreply.github.com>
2025-06-29 00:04:38 +0300

    Update save_it.py
    some changes to make it work on Termux and Python 3.12

--- core/save_it.py
+++ core/save_it.py
@@ -1,27 +1,18 @@
 import os
 import errno
 
-
-def save_func(final_urls , outfile , domain):
+def save_func(final_urls, outfile, domain):
     if outfile:
-        if "/" in outfile:
-            filename = f'{outfile}'
-        else : 
-            filename = f'output/{outfile}'
-    else :
+        filename = outfile if "/" in outfile else f"output/{outfile}"
+    else:
         filename = f"output/{domain}.txt"
-    
-    if os.path.exists(filename):
-        os.remove(filename)
 
-    if not os.path.exists(os.path.dirname(filename)):
-        try:
-            os.makedirs(os.path.dirname(filename))
-        except OSError as exc: 
-            if exc.errno != errno.EEXIST:
-                raise
-    
-    
-    for i in final_urls:
-        with open(filename, "a" , encoding="utf-8") as f:
-            f.write(i+"\n")
+    try:
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise
+
+    with open(filename, "w", encoding="utf-8") as f:
+        for i in final_urls:
+            f.write(i + "\n")
